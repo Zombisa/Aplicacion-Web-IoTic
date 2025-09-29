c8b245e4975217412f4f6d1231b4b6e8d0a26fa6
Isabela Sánchez Saavedra <isanchez@unicauca.edu.co>
2025-09-29 08:35:34 -0500

    initial commit
    - Added base Spring Boot backend project (empty)
    - Created frontend directory (placeholder)

---
+++ Backend/iotsemillero-backend/src/main/java/edu/unicauca/iotic/iotsemillero_backend/IotsemilleroBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.unicauca.iotic.iotsemillero_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotsemilleroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotsemilleroBackendApplication.class, args);
+	}
+
+}
