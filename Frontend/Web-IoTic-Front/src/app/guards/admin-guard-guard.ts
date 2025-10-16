697270e248ba0c62876c60a8a4b2afc67ae7bdd4
Santiago Escandon <santiescandon1@gmail.com>
2025-10-15 21:56:37 -0500

    se modifica el guardian de administrador, se desea separar la implementacion de los guardianes, pimer avance

--- Frontend/Web-IoTic-Front/src/app/guards/admin-guard-guard.ts
+++ Frontend/Web-IoTic-Front/src/app/guards/admin-guard-guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { Observable } from 'rxjs/internal/Observable';
-import { map, take } from 'rxjs';
+import { map, switchMap, take } from 'rxjs';
 
     @Injectable({
       providedIn: 'root'
@@ -11,20 +11,24 @@ import { map, take } from 'rxjs';
 
       constructor(private authService: AuthService, private router: Router) {}
 
-      canActivate(
-        route: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
-        return this.authService.isAdmin().pipe(
-          take(1), // Toma solo el primer valor y luego completa el Observable
-          map(isAdmin => {
-            if (isAdmin) {
-              return true; // Permitir acceso a la ruta
-            } else {
-              // Redirigir al usuario a una página de acceso denegado o a la página de inicio
-              console.warn('Acceso denegado: El usuario no es administrador.');
-              return this.router.createUrlTree(['/access-denied']); // O '/home'
+      canActivate(): Observable<boolean | UrlTree> {
+        return this.authService.currentUser.pipe(
+          take(1),
+          switchMap(user => {
+            if (!user) {
+              console.warn('Usuario no autenticado, redirigiendo al login...');
+              return [this.router.createUrlTree(['/login'])];
             }
+            return this.authService.isAdmin().pipe(
+              map(isAdmin => {
+                if (isAdmin) {
+                  return true;
+                } else {
+                  console.warn('Acceso denegado: no es administrador.');
+                  return this.router.createUrlTree(['/access-denied']);
+                }
+              })
+            );
           })
         );
       }
