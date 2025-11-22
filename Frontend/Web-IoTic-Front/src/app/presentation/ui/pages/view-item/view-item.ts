715b7651061c26eda11e7e48bd2a222c0e7408a5
Santiago Escandon <santiescandon1@gmail.com>
2025-11-22 17:25:56 -0500

    mejora de add-item en css

--- Frontend/Web-IoTic-Front/src/app/presentation/ui/pages/view-item/view-item.ts
+++ Frontend/Web-IoTic-Front/src/app/presentation/ui/pages/view-item/view-item.ts
@@ -5,10 +5,12 @@ import { Header } from '../../templates/header/header';
 import { ItemDTO } from '../../../../models/DTO/ItemDTO';
 import { ActivatedRoute, Router } from '@angular/router';
 import { InventoryService } from '../../../../services/inventory.service';
+import { LoadingService } from '../../../../services/loading.service';
+import { LoadingPage } from '../../components/loading-page/loading-page';
 
 @Component({
   selector: 'app-view-item',
-  imports: [CommonModule, Header],
+  imports: [CommonModule, Header, LoadingPage],
   templateUrl: './view-item.html',
   styleUrl: './view-item.css'
 })
@@ -19,6 +21,7 @@ export class ViewItem implements OnInit {
   constructor(
     private inventoryService: InventoryService,
     private activatedRoute: ActivatedRoute,
+    public loadingService: LoadingService,
     private router: Router
   ) {}
   /**
@@ -26,6 +29,7 @@ export class ViewItem implements OnInit {
    * Obtiene el id de la url
    */
   ngOnInit(): void {
+    
     this.activatedRoute.paramMap.subscribe(params => {
       this.itemId = Number(params.get('id'));
       console.log('ID del item obtenido de la URL:', this.itemId);
@@ -38,16 +42,19 @@ export class ViewItem implements OnInit {
    * @Returns void no retorna el item pero si lo guarda dentro de la variable del componente
    */
   getItemById(): void {
+    this.loadingService.show();
     console.log('Obteniendo el item con ID:', this.itemId);
     this.inventoryService.getElectronicComponentById(this.itemId).subscribe({
       next: (item) => {
         this.item = item;
         console.log('Item obtenido:', this.item);
+        this.loadingService.hide();
       },
       error: (error) => {
         console.error('Error al obtener el item:', error);
       }
     })  
+    
   };
   /**
    * Navegar a la página de edición del item
