feb557c474ecc1935a68da9bf60e2609dfd3aa2e
sonhuila <mariapaulabm09@unicauca.edu.co>
2025-12-10 16:04:41 -0500

    se agregan modelo DTO del registro fotografico

---
+++ Frontend/Web-IoTic-Front/src/app/models/DTO/RegistroFotograficoDTO.ts
@@ -0,0 +1,8 @@
+export interface RegistroFotograficoDTO {
+  id: number;
+  usuario: number; 
+  titulo?: string | null;
+  fecha?: string | null; 
+  descripcion?: string | null;
+  foto_r2: string; 
+}
---
+++ Frontend/Web-IoTic-Front/src/app/models/Peticion/RegistroFotograficoPeticion.ts
@@ -0,0 +1,6 @@
+export interface RegistroFotograficoPeticion {
+  titulo?: string | null;
+  fecha?: string | null;
+  descripcion?: string | null;
+  foto_r2: string; 
+}
