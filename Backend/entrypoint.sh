10167d7ce5044b85358351232fe66f1731999ad7
Santiago Escandon <santiescandon1@gmail.com>
2025-12-16 23:29:50 -0500

    correcion formato

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -1,4 +1,3 @@
-  GNU nano 7.2                                                                                                                      entrypoint.sh                                                                                                                               
 #!/bin/sh
 set -e
 
